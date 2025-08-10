859676f6edae039cd58ace0512efabdd244af710
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-08-10 13:19:40 +0200

    Implemented data_loader logic.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
-def load_emotion_data(path):
-    return pd.read_csv(path)
+def load_datasets():
+    emotion_df = pd.read_csv("data/emotions.csv")
+    violence_df = pd.read_csv("data/violence.csv")
+    gbv_df = pd.read_csv("data/gbv_hate_speech.csv")
 
-def load_gbv_data(path):
-    return pd.read_csv(path)
+    # Clean and align structure
+    emotion_df.drop(columns=['Unnamed: 0'], inplace=True)
+    violence_df.drop(columns=['Tweet_ID'], inplace=True)
+    gbv_df = gbv_df[['tweet', 'class']]
 
-def load_violence_data(path):
-    return pd.read_csv(path)
+    # Rename columns
+    violence_df.rename(columns={'tweet': 'text', 'type': 'label'}, inplace=True)
+    gbv_df.rename(columns={'tweet': 'text', 'class': 'label'}, inplace=True)
+
+    return emotion_df, violence_df, gbv_df
